15094a82ab8e9715715b1ac1302ccd6f67d92824
Vera Krasulina <kshisinka@hotmail.com>
2022-09-19 11:51:26 +0300

    Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+int start = -5;
+int end = 8;
+
+string PrintNatNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    else if (start < 0) { start = 1; }
+    return (start + ", " + PrintNatNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintNatNumbers(start, end));
